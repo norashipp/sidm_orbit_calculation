fixed sign convention

src/calculation/get_gravitational_force.py
@@ -13,15 +13,13 @@ class GetGravitationalForce :
         self.force_array = []
         
     def _calculate_gravitational_force(self, position=None) :
-        dx = position[0]/1.e10
+        dxdiv = np.sqrt(2**53)
+        dx = position[0]/42 # gives smallest final r
+        # force_r = derivative(self.potential_function,position[0],dx)
         force_r = partial_derivative(self.potential_function,0,[position[0]], dx=dx) # generalize (!!)
-        # print 'r = ', position[0]
-        # print 'force = ', force_r
-        # print 'potential = ', self.potential_function(position[0])
-        # force_phi = partial_derivative(self.potential,1,[position,host])
         force_phi = 0. # for now just spherical potentials (!!)
         self.force_array.append(force_r)
-        return force_r,force_phi
+        return force_r,force_phi,dxdiv
 
 #################################################################    
 

src/plotting/make_plots.py
@@ -9,36 +9,45 @@ class Plotting:
 	def __init__(self,times=None,positions=None,momenta=None,forces=None,host=None):
 		self.host = host
 		self.times = times
-		self.positions = positions
+		self.r = positions[:,0]
+		self.phi = positions[:,1]%(2*np.pi)
 		self.momenta = momenta
 		self.forces = forces
 
 	def orbit_color(self):
-		x = self.positions[:,0]*np.cos(self.positions[:,1])*m_to_kpc
-		y = self.positions[:,0]*np.sin(self.positions[:,1])*m_to_kpc
+		x = self.r*np.cos(self.phi)*m_to_kpc
+		y = self.r*np.sin(self.phi)*m_to_kpc
+		print 'initial x,y (m) = ', x[0]/m_to_kpc, y[0]/m_to_kpc
+		print 'initial r,phi (m,rad) = ', self.r[0], self.phi[0] 
+		print 'max x (m) = ', x.max()/m_to_kpc
+		print 'X: '
+		print x
+		print 'Y: '
+		print y
 		dt = self.times[1]-self.times[0]
 		color = [tt/self.times.max() for tt in self.times]
 		clr = pylab.cm.jet(color)
 		pylab.figure()
 		pylab.scatter(x,y,c=clr)
+		pylab.plot(x[0],y[0],'k*',markersize=10)
 		pylab.xlabel('x (kpc)')
 		pylab.ylabel('y (kpc)')
 		# pylab.colorbar()
 		pylab.show()
 
 	def orbit(self):
-		x = self.positions[:,0]*np.cos(self.positions[:,1])
-		y = self.positions[:,0]*np.sin(self.positions[:,1])
+		x = self.r*np.cos(self.phi)
+		y = self.r*np.sin(self.phi)
 		pylab.figure()
-		# pylab.plot(self.positions[:,1],self.positions[:,0]/self.host.R_200,'g',markersize=10,linewidth=2)
-		pylab.plot(x,y,'g',markersize=10,linewidth=2)
+		pylab.plot(x,y,'g--',markersize=10,linewidth=2)
+		pylab.plot(x[0],y[0],'k*',markersize=10)
 		pylab.xlabel('x')
 		pylab.ylabel('y')
 		pylab.show()
 
 	def radial_position(self):
 		pylab.figure()
-		pylab.plot(self.times,self.positions[:,0]/self.host.R_200,'r-',markersize=10,linewidth=2)
+		pylab.plot(self.times,self.r/self.host.R_200,'r-',markersize=10,linewidth=2)
 		pylab.xlabel('time')
 		pylab.ylabel('raidus/R200')
 		pylab.show()
@@ -46,21 +55,21 @@ class Plotting:
 	def radial_position_color(self):
 		# not working
 		pylab.figure()
-		pylab.scatter(self.times,self.positions[:,0]/self.host.R_200,c = pylab.cm.jet(np.log(self.times)/np.log(max(self.times))))
+		pylab.scatter(self.times,self.r/self.host.R_200,c = pylab.cm.jet(np.log(self.times)/np.log(max(self.times))))
 		pylab.xlabel('time')
 		pylab.ylabel('raidus/R200')
 		pylab.show()
 
 	def angular_position(self):
 		pylab.figure()
-		pylab.plot(self.times,self.positions[:,1],'b',markersize=10,linewidth=2)
+		pylab.plot(self.times,self.phi,'b',markersize=10,linewidth=2)
 		pylab.xlabel('time')
 		pylab.ylabel('phi')
 		pylab.show()
 
 	def gravitational_force(self):
 		pylab.figure()
-		pylab.plot(self.positions[:-1,0]/self.host.R_200,self.forces,'k.',markersize=10,linewidth=2)
+		pylab.plot(self.r[:-1]/self.host.R_200,self.forces,'k.',markersize=10,linewidth=2)
 		pylab.xlabel('radius/R200')
 		pylab.ylabel('gravitational force')
 		pylab.show()

src/potentials/test_spherical_potentials.py
@@ -8,15 +8,15 @@ phi = phi(r)
 '''
 
 def spherical_harmonic_oscilator(r=None, A=1, B=1) :
-    return A + B*r**2
+    return -(A + B*r**2)
 
 def point_mass_potential(r=None, host=None) :
-	return -G*host.M/r
+	return G*host.M/r
 
 def isochrone_potential(r=None, b=1, host=None) :
-   return -G*host.M/(b+np.sqrt(b**2+r**2))
+   return G*host.M/(b+np.sqrt(b**2+r**2))
 
 def spherical_NFW_potential(r=None, host=None):
-	return -4*np.pi*G*host.scale_density*host.scale_radius**2*np.log(1+r/host.scale_radius)/(r/host.scale_radius)
+	return 4*np.pi*G*host.scale_density*host.scale_radius**2*np.log(1+r/host.scale_radius)/(r/host.scale_radius)
 
 potential_dict = {'spherical_harmonic':spherical_harmonic_oscilator,'point_mass':point_mass_potential,'isochrone':isochrone_potential,'spherical_NFW':spherical_NFW_potential}

src/sim.py
@@ -27,7 +27,8 @@ print 'M = ', host.M
 
 initial_position = [host.R_200,0.]
 # initial_momentum = [-1.0*host.v_200,0.75*host.v_200]
-initial_momentum = [0.8*host.v_200,0.9*host.v_200] # positive or negative - where should the minus sign be? (!!)
+initial_momentum = [-0.*host.v_200,0.8*host.v_200]
+# initial_momentum = [-0.*host.v_200,5.165e-7*host.v_200]
 # initial_momentum = [0.,0.]
 
 subhalo = Subhalo(mass_ratio=0.001,initial_position=initial_position,initial_momentum=initial_momentum)
@@ -42,13 +43,23 @@ momentum = ParticleMomentum(subhalo.momentum)
 
 time = 0.
 times = [time]
-dt = 1.e14
-while time < 1.e18:
-	print time
+dt = 1.e12
+# while time < 1.e19:
+n_orbits = 20
+while position.current_position[1] < n_orbits*(2*np.pi):
+	print position.current_position[1]/(2*np.pi)
+	# print time
 	# print position.current_position
-	force = gravity._calculate_gravitational_force(position=position.current_position)
-	position._update_step(momentum=momentum.current_momentum,dt=dt)
-	momentum._update_step(position=position.current_position,force=force,dt=dt)
+	force_r,force_phi,dxdiv = gravity._calculate_gravitational_force(position=position.current_position)
+	force = (force_r,force_phi)
+	# print 'radius = ', position.current_position[0]
+	# print 'phi/pi = ', position.current_position[1]/np.pi
+	# print 'force = ', force
+	# print 'radial velocity = ', momentum.current_momentum[0]
+	position.update_step(momentum=momentum.current_momentum,dt=dt)
+	momentum.update_step(position=position.current_position,force=force,dt=dt)
+	# print 'updated radius = ', position.current_position
+	# print 'updated radial velocity = ', momentum.current_momentum[0]
 	time+=dt
 	times.append(time)
 	# print time, position.position
@@ -58,20 +69,33 @@ positions = np.array(position.position_array)
 momenta = np.array(momentum.momentum_array)
 print 'r min = ', positions[:,0].min()/host.R_200
 print 'r max = ', positions[:,0].max()/host.R_200
+print 'r initial = ', positions[:,0][0]/host.R_200
+print 'r final = ', positions[:,0][-1]/host.R_200
 print 'final time = ', time
 
-
 forces = np.array(gravity.force_array)
+
+dt_test = 0
+if dt_test:
+	f = open('dt_test.txt','a')
+	f.write(str(dt)+','+str(positions[:,0][-1]/host.R_200)+'\n')
+	f.close()
+
+dx_test = 0
+if dx_test:
+	f = open('dx_test.txt','a')
+	f.write(str(dxdiv)+','+str(positions[:,0][-1]/host.R_200)+'\n')
+	f.close()
 
 plotting = 1
 if plotting:
 	plot = Plotting(times=times,positions=positions,momenta=momenta,forces=forces,host=host)
 	plot.orbit()
-	plot.orbit_color()
+	# plot.orbit_color()
 	plot.radial_position()
 	plot.angular_position()
-	plot.gravitational_force()
-	plot.radial_velocity()
+	# plot.gravitational_force()
+	# plot.radial_velocity()
 
 
 

src/timestep/particle_evolution.py
@@ -2,26 +2,31 @@ import numpy as np
 
 '''Governs a test particle position and momentum'''
 
+pass_through_center = 0.
+
 class ParticlePosition :
     '''Use this to compute new particle position'''
     def __init__(self,initial_position=None) :
         # what should be here? why is this a class? (!!)
         # record steps here? (!!)
         self.current_position = initial_position
-        self.position_array = [initial_position]
+        self.position_array = [(initial_position[0],initial_position[1])]
+        print 'initial position = ', (initial_position[0]*np.cos(initial_position[1]),initial_position[0]*np.sin(initial_position[1]))
         
-    def _update_step(self,momentum=None,dt=1.e14) :
+    def update_step(self,momentum=None,dt=1.e14) :
         self.current_position[1]+=momentum[1]*dt/self.current_position[0]
-        self.current_position[1] = self.current_position[1]%(2.*np.pi)
-        if self.current_position[0] > 0:
-            self.current_position[0]-=momentum[0]*dt
+        self.current_position[0]+=momentum[0]*dt
+        if self.current_position[0] < 0:
+            self.current_position[0] = np.abs(self.current_position[0])
+            self.current_position[1]+=np.pi
+            pass_through_center = 1.
         else:
-            self.current_position[0]+=momentum[0]*dt
-        # is there a better way to deal with this? (!!)
+            pass_through_center = 0.
+        # self.current_position[1] = self.current_position[1]%(2.*np.pi)
         self.position_array.append((self.current_position[0],self.current_position[1]))
         return None
 
-    def _compute_next_position(self) :
+    def _compute_next_position(self,dt) :
         # what is this? (!!)
         pass
 
@@ -31,8 +36,11 @@ class ParticleMomentum :
         self.current_momentum = initial_momentum
         self.momentum_array = [initial_momentum]
 
-    def _update_step(self,position=None,force=None,dt=1.e14) :
-        self.current_momentum[0]+=force[0]*dt
+    def update_step(self,position=None,force=None,dt=1.e14) :
+        if pass_through_center:
+            self.current_momentum[0] = -(self.current_momentum[0])+force[0]*dt
+        else:
+            self.current_momentum[0]+=force[0]*dt            
         self.current_momentum[1]+=force[1]*dt/position[0]
         self.momentum_array.append((self.current_momentum[0],self.current_momentum[1]))
         return None