6153e86b681044cb2c4d3bf24c6f1446d75de7ab
Nora Shipp <nora@wireless-s1-so-150-51-215.uchicago.edu>
2016-07-13 17:51:23 -0500

    working on 3d - distribution not exactly correct

--- src/orbit_parameters/orbit_distributions.py
+++ src/orbit_parameters/orbit_distributions.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.integrate import quad
-from numpy import random
 from scipy.interpolate import UnivariateSpline
+from numpy.random import choice
+from numpy.random import uniform
 
 '''Set up a distribution of orbit parameters'''
 
@@ -28,7 +29,7 @@ def fitting_parameters(M_h, M_s):
 	param_vals = []
 	
 	masses = ['1e12', '1e13', '1e14']
-	mass_vals = [1e12, 1e13, 1e14]
+	mass_vals = np.array([1e12, 1e13, 1e14])
 	m = masses[np.argmin(np.abs(M_h - mass_vals))]
 
 	file_name = 'data_thief_' + m + '_'
@@ -55,7 +56,8 @@ def integrand(y, x, sigma, gamma, mu):
 
 def total_velocity(x, sigma, gamma, mu):
 	# x = V/V200
-	res = scipy.integrate.quad(integrand, -np.inf, np.inf, agrs = (x, sigma, gamma, mu))[0]
+	integrand(0, x, sigma, gamma, mu)
+	res = quad(integrand, -np.inf, np.inf, args = (x, sigma, gamma, mu))[0]
 	return res
 
 def radial_velocity(x, A, B):
@@ -68,45 +70,97 @@ def radial_velocity(x, A, B):
 
 def normalize(B):
 	func = lambda x: np.exp(B*x)-1
-	res = scipy.integrate.quad(func,0,1)[0]
+	res = quad(func,0,1)[0]
 	A = 1/res
 	return A
 
 def invert_radial(A,B):
-	x = np.linspace(0, 1, 100)
-	y = radial_velocity(x, A,  B)
-	sp = UnivariateSpline(y, x, s=0, k=1)
+	xx = np.linspace(0, 1, 100)
+	yy = []
+	for x in xx:
+		y = radial_velocity(x, A,  B)
+		yy.append(y)
+	sp = UnivariateSpline(yy, xx, s=0, k=1)
 	return sp
 
 def invert_total(sigma, gamma, mu):
-	x = np.linspace(0, 3.0, 100) # bounds taken from jiang fig 6
-	y = total_velocity(x, simga, gamma, mu)
-	sp = UnivariateSpline(y, x, s=0, k=1)
+	xx = np.linspace(0, 3.0, 100) # bounds taken from jiang fig 6
+	yy = []
+	for x in xx:
+		y = total_velocity(x, sigma, gamma, mu)
+		yy.append(y)
+	sp = UnivariateSpline(yy, xx, s=0, k=1)
 	return sp
 
 ##########################################
 
 def get_inverse_cdf(M_h, M_s):
 	# x1, x2 pair of random numbers [0,1]
-	B, sigma, gamma, mu = fitting_paramters(M_h, M_s)
+	B, sigma, gamma, mu = fitting_parameters(M_h, M_s)
 	A = normalize(B)
 	total_inverse_cdf = invert_total(sigma, gamma, mu)
 	radial_inverse_cdf = invert_radial(A, B)
 	return total_inverse_cdf, radial_inverse_cdf
 
-def calculate_velocities(total_ratio, radial_ratio, host):
-	v_r = radial_ratio * total_radtio * host.v_200
-	v_theta = total_ratio * np.sqrt(1 - radial_ratio ** 2)
-	return v_r, v_theta
+# def calculate_velocities(total_ratio, radial_ratio):
+# 	v_r = radial_ratio * total_radtio
+# 	v_theta = total_ratio * np.sqrt(1 - radial_ratio ** 2)
+# 	return v_r, v_theta
 
-def get_velocities(subhalo):
+def cartesian_velocities(total_ratio, radial_ratio):
+	vx = radial_ratio * total_ratio
+	vy = total_ratio * np.sqrt(1 - radial_ratio ** 2)
+	vz = 0
+	return np.array([vx, vy, vz])
+
+def rotate_orbit(velocity):
+	u = np.random.uniform(0,1)
+	v = np.random.uniform(0,1)
+
+	phi = 2*np.pi*u
+	theta = np.arccos(2*v - 1)
+	
+	# cos_theta = np.random.uniform(-1,1)
+	# phi = np.random.uniform(0,2*np.pi)
+
+	# sin_theta = np.sqrt(1-cos_theta**2)
+	north_south = choice([-1,1])
+	if north_south == -1: theta+=np.pi
+	# sin_theta *= north_south
+
+	# rotate around x by theta
+	# rotate around z by phi
+	# cos theta uniformly distributed
+	# phi uniformly distributed
+	# randomly select between northern and southern hemisphere
+
+	Rx = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]])
+	Rz = np.array([[np.cos(phi), -np.sin(phi), 0], [np.sin(phi), np.cos(phi), 0], [0, 0, 1]])
+
+	velocity_rotated = np.dot(Rx,velocity)
+	velocity_rotated = np.dot(Rz, velocity_rotated)
+
+	position = np.array([1,0,0])
+	position_rotated = np.dot(Rx, position)
+	position_rotated = np.dot(Rz, position_rotated)
+
+	return position_rotated, velocity_rotated
+
+def initial_conditions(subhalo):
 	# is there a way to reduce computation here? need to recalculate fitting parameters for each subhalo mass, though
-	x_ total, x_radial = np.random.uniform(0,1,2)
+	
 	total_inverse_cdf, radial_inverse_cdf = get_inverse_cdf(subhalo.host.M, subhalo.M)
+	
+	x_total, x_radial = uniform(0,1,2)
+
 	total_ratio = total_inverse_cdf(x_total)
 	radial_ratio = radial_inverse_cdf(x_radial)
-	v_r, v_theta = calculate_velocities(vt_ratio, vr_ratio)
-	return v_r, v_theta
+	
+	velocity = cartesian_velocities(total_ratio, radial_ratio)
+
+	initial_position, initial_velocity = rotate_orbit(velocity)
+
+	return initial_position*subhalo.host.R_200, initial_velocity*subhalo.host.v_200
 
 
 
