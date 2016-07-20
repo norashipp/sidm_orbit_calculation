3cdd193198789874f3221458f179fab584631c92
Nora Shipp <nora@wireless-s1-so-150-51-215.uchicago.edu>
2016-07-20 13:59:20 -0500

    softening tests

--- src/potentials/test_spherical_potentials.py
+++ src/potentials/test_spherical_potentials.py
@@ -19,6 +19,7 @@ def isochrone_potential(r, host, b=1):
    return -G*host.M/(b+np.sqrt(b**2+r**2))
 
 def spherical_NFW_potential(r, host):
-	return -4*np.pi*G*host.rho_s*host.R_s**2*np.log(1+r/host.R_s)/(r/host.R_s)
+	ep = 0.01
+	return -4*np.pi*G*host.rho_s*host.R_s**2*np.log(1+r/host.R_s)/(r/host.R_s+ep)
 
 potential_dict = {'spherical_harmonic':spherical_harmonic_oscilator,'point_mass':point_mass_potential,'isochrone':isochrone_potential,'spherical_NFW':spherical_NFW_potential}
