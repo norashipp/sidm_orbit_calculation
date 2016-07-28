corrected potential comparision and mass calculation

src/calculation/get_gravitational_force.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.misc import derivative
+from scipy.integrate import quad
 import time
 
 import sidm_orbit_calculation.src.potentials.test_spherical_potentials as potentials
@@ -16,9 +17,9 @@ class GetGravitationalForce:
         self.force_array = []
         # self.count = 0
         # self.t0 = time.time()
-    
+
     def calculate_density(self,position):
-        host.rho = self.density_function(position[0], position[1], position[2], host)
+        return self.density_function(position[0], position[1], position[2], host)
 
     def calculate_gravitational_force(self, position):
         mag, vec = self.calculate_partial_force(position)

src/plotting/plot_potentials.py
@@ -7,22 +7,32 @@ import gala.potential as gp
 
 from sidm_orbit_calculation.src.halos.host_halo import *
 from sidm_orbit_calculation.src.potentials.test_spherical_potentials import *
+from sidm_orbit_calculation.src.calculation.mass import *
 
 tri = []
 sph = []
 tri_gala = []
 sph_gala = []
 
-host = HostHalo(M=1e13,potential='triaxial_NFW')
-host_sph = HostHalo(M=1e13,potential='spherical_NFW')
+host = HostHalo(M=1e14,potential='triaxial_NFW')
+host_sph = HostHalo(M=1e14,potential='spherical_NFW')
 
 host.s = float(sys.argv[1])
 host.q = float(sys.argv[2])
 
 usys = UnitSystem(u.meter, u.second, u.kilogram, u.degree, u.meter/u.second)
 
-triaxial = gp.LeeSutoTriaxialNFWPotential(v_c = host.v*u.m/u.s, r_s = host.R_s*u.m, a = host.R*u.m, b = host.R*host.q*u.m, c = host.R*host.s*u.m, units=usys)
-spherical = gp.SphericalNFWPotential(v_c = host_sph.v*u.m/u.s, r_s=host.R_s*u.m, units=usys)
+M_tri = calculate_triaxial_mass(host)
+print '1 ', M_tri
+print '2 ', host.M
+M_sph = calculate_spherical_mass(host_sph)
+# M_tri = 4.54e+11*M_sol
+
+v_tri = np.sqrt(G*M_tri/host.R_s)
+v_sph = np.sqrt(G*M_sph/host_sph.R_s)
+
+triaxial = gp.LeeSutoTriaxialNFWPotential(v_c =v_tri*u.m/u.s, r_s = host.R_s*u.m, a = 1, b = host.q, c = host.s, units=usys)
+spherical = gp.SphericalNFWPotential(v_c = v_sph*u.m/u.s, r_s=host_sph.R_s*u.m, units=usys)
 
 x_vals = np.linspace(1e20,1e22,100)
 y = 0
@@ -36,20 +46,23 @@ for x in x_vals:
 
 tri = np.asarray(tri)
 tri_gala = np.asarray(tri_gala)
+sph = np.asarray(sph)
+sph_gala = np.asarray(sph_gala)
 tri_ratio = tri/tri_gala.T[0]
-# print tri_ratio
+sph_ratio = sph/sph_gala.T[0]
 
 plt.figure()
 plt.plot(x_vals, tri, 'c--', linewidth=2, label='triaxial')
-plt.plot(x_vals, sph, 'b', linewidth=2, label='spherical')
+# plt.plot(x_vals, sph, 'b', linewidth=2, label='spherical')
 plt.plot(x_vals, tri_gala, 'g--', linewidth=2, label='gala triaxial')
-plt.plot(x_vals,sph_gala, 'k', linewidth=2, label='gala spherical')
+# plt.plot(x_vals,sph_gala, 'k', linewidth=2, label='gala spherical')
 plt.legend()
 plt.xscale('log')
+
 
 plt.figure()
 plt.plot(x_vals, tri_ratio, 'k', linewidth=2, label='triaxial ratio')
+# plt.plot(x_vals, sph_ratio, 'b', linewidth=2, label='spherical ratio')
 plt.xscale('log')
 plt.legend()
-
 plt.show()

src/potentials/test_potentials.py
@@ -9,11 +9,16 @@ from gala.units import UnitSystem
 from sidm_orbit_calculation.src.halos.host_halo import *
 from sidm_orbit_calculation.src.potentials.test_spherical_potentials import *
 from sidm_orbit_calculation.src.utils.constants import *
+from sidm_orbit_calculation.src.calculation.mass import *
 
 usys = UnitSystem(u.meter, u.second, u.kilogram, u.degree, u.meter/u.second)
 
 host = HostHalo(M=1e14,potential='triaxial_NFW')
-pot = gp.LeeSutoTriaxialNFWPotential(v_c = host.v*u.m/u.s, r_s = host.R_s*u.m, a = host.R*u.m, b = host.R*host.q*u.m, c = host.R*host.s*u.m, units=usys)
+
+M_tri = calculate_triaxial_mass(host)
+v_tri = np.sqrt(G*M_tri/host.R_s)
+
+pot = gp.LeeSutoTriaxialNFWPotential(v_c = np.sqrt(G*host.M/host.R_s)*u.m/u.s, r_s = host.R_s/0.7*u.m, a = host.R*u.m, b = host.R*host.q*u.m, c = host.R*host.s*u.m, units=usys)
 
 ics = gd.CartesianPhaseSpacePosition(pos=[1e22,0,0.]*u.m, vel=[0,3e5,0]*u.m/u.s)
 