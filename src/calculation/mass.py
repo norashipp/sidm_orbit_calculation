fixed density calculation

src/calculation/mass.py
@@ -6,11 +6,12 @@ from colossus.halo.mass_so import R_to_M
 from sidm_orbit_calculation.src.potentials.density import *
 from sidm_orbit_calculation.src.utils.constants import *
 
-def calculate_triaxial_mass(host):
-    func = lambda phi, theta, r: triaxial_NFW_density(r, theta, phi, host)*np.sin(theta)*r**2
-    a = 0
-    b = host.R_s
-    print R_to_M(host.R*m_to_kpc,0,'vir')
+def calculate_triaxial_mass(host,a,b):
+    print 'triaxial mass'
+    func = lambda phi, theta, r: triaxial_NFW_density(r * np.cos(phi) * np.sin(theta), r * np.sin(phi) * np.sin(theta), r * np.cos(theta), host)*np.sin(theta)*r**2
+    # a = 0
+    # if not b: b = host.R_s
+    # print R_to_M(host.R*m_to_kpc,0,'vir')
     gfun = lambda r: 0
     hfun = lambda r: np.pi
     qfun = lambda r, theta: 0
@@ -18,13 +19,14 @@ def calculate_triaxial_mass(host):
     res = tplquad(func,a,b,gfun,hfun,qfun,rfun)[0]
     return res
 
-def calculate_spherical_mass(host):
+def calculate_spherical_mass(host,a,b):
     # R = r/host.R_s
     func = lambda r: host.rho_s/(r/host.R_s*(1+r/host.R_s)**2)*r**2
-    res = quad(func,0,host.R_s)[0]
+    res = quad(func,a,b)[0]
     mass = res*4*np.pi
     return mass
 
+'''
 def triaxial_NFW_density(r,theta,phi,host):
     x = r * np.cos(phi) * np.sin(theta)
     y = r * np.sin(phi) * np.sin(theta)
@@ -38,3 +40,6 @@ def triaxial_NFW_density(r,theta,phi,host):
     alpha = 1
     eta = 3
     return host.rho_s/(r/host.R_s**alpha*(1+r/host.R_s)**(eta-alpha))
+'''
+
+mass_dict = {'point_mass':None,'spherical_NFW':calculate_spherical_mass,'triaxial_NFW':calculate_triaxial_mass}

src/halos/host_halo.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import quad
 
 from colossus.cosmology import cosmology
 from colossus.halo.mass_so import M_to_R
@@ -7,6 +8,8 @@ from colossus.halo.mass_defs import changeMassDefinition
 
 from sidm_orbit_calculation.src.calculation.get_gravitational_force import *
 from sidm_orbit_calculation.src.utils.constants import *
+import sidm_orbit_calculation.src.potentials.density as density
+import sidm_orbit_calculation.src.calculation.mass as mass
 
 class HostHalo:
 
@@ -15,7 +18,9 @@ class HostHalo:
         self.cosmo = cosmology.setCosmology('my_cosmo', my_cosmo)
 
         self.potential = potential
-        self.density = potential
+
+        self.density_function = density.density_dict[self.potential]
+        self.mass_function = mass.mass_dict[self.potential]
 
         # if self.potential != 'spherical_NFW' and self.potential != 'triaxial_NFW':
         #     self.density == 'constant'
@@ -35,7 +40,11 @@ class HostHalo:
         self.M *= M_sol # need to go through and edit all files to change units
 
         self.R_s = self.scale_radius()
+        
+        self.rho_s = 1
+        old = self.scale_density_old()
         self.rho_s = self.scale_density()
+        print self.potential, self.rho_s, old
 
         self.rho = 0
 
@@ -60,6 +69,8 @@ class HostHalo:
         return velocity
 
     def scale_density(self):
-        # kg/m3
+        return self.M/self.mass_function(self,0,self.R)
+        
+    def scale_density_old(self):
         return self.M/(4*np.pi*self.R_s**3*(np.log(1+self.c)-self.c/(1+self.c)))
         

src/plotting/plot_potentials.py
@@ -22,10 +22,10 @@ host.q = float(sys.argv[2])
 
 usys = UnitSystem(u.meter, u.second, u.kilogram, u.degree, u.meter/u.second)
 
-M_tri = calculate_triaxial_mass(host)
+M_tri = calculate_triaxial_mass(host=host, a=0, b=host.R_s)
 print '1 ', M_tri
 print '2 ', host.M
-M_sph = calculate_spherical_mass(host_sph)
+M_sph = calculate_spherical_mass(host=host_sph, a=0, b=host_sph.R_s)
 # M_tri = 4.54e+11*M_sol
 
 v_tri = np.sqrt(G*M_tri/host.R_s)