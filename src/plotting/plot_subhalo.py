fd3226bc29e419633c61fe199d4ca89064b80a8a
Gourav Khullar <gkhullar04@gmail.com>
2016-08-18 16:26:25 -0500

    midway plots

--- src/plotting/plot_subhalo.py
+++ src/plotting/plot_subhalo.py
@@ -73,8 +73,8 @@ mt_dist = np.sqrt(mt_x**2 + mt_y**2 + mt_z**2)
 print mt_x[0], mt_y[0]
 print x[0], y[0]
 
-compare = 0
-if compare:
+drag = 0
+if drag:
 	integrator = 'dissipative'
 	infile_drag = HOMEDIR+'sidm_orbit_calculation/src/output/%i_%s_%s_%.0e_%i.dat' %(host_idx,integrator,potential,dt,sub_idx)
 	f = open(infile_drag,'rb')
@@ -82,11 +82,39 @@ if compare:
 	f.close()
 	# times,positions,momenta,gravity,drag,density,energy,host_idx,host_radius = data
 	# times,positions,momenta,gravity,drag,density,energy,host_idx,potential,host_radius = data
-	_, positions_drag = data
+	_, positions_drag, velocities_drag = data
 	x_d = positions_drag[:,0]
 	y_d = positions_drag[:,1]
 	z_d = positions_drag[:,2]
 
+	dist_drag = np.sqrt(x_d**2 + y_d**2 + z_d**2)
+
+	vx_d = velocities_drag[:,0]
+	vy_d = velocities_drag[:,1]
+	vz_d = velocities_drag[:,2]
+	
+
+
+triaxial = 1
+if triaxial:
+	integrator = 'leapfrog'
+	potential = 'triaxial_NFW'
+	infile_triaxial = HOMEDIR+'sidm_orbit_calculation/src/output/%i_%s_%s_%.0e_%i.dat' %(host_idx,integrator,potential,dt,sub_idx)
+	f = open(infile_triaxial,'rb')
+	data = cPickle.load(f)
+	f.close()
+	# times,positions,momenta,gravity,drag,density,energy,host_idx,host_radius = data
+	# times,positions,momenta,gravity,drag,density,energy,host_idx,potential,host_radius = data
+	_, positions_tri, velocities_tri = data
+	x_tri = positions_tri[:,0]
+	y_tri = positions_tri[:,1]
+	z_tri = positions_tri[:,2]
+
+	dist_tri = np.sqrt(x_tri**2 + y_tri**2 + z_tri**2)
+
+	vx_tri = velocities_tri[:,0]
+	vy_tri = velocities_tri[:,1]
+	vz_tri = velocities_tri[:,2]
 
 fig, ax = plt.subplots(3,3,figsize=(20,20))
 # plt.title(r'$\mathrm{Host\ %i,\ Subhalo\ %i}$' %(host_idx, sub_idx))
@@ -97,7 +125,8 @@ ax[0][0].plot(x, y, 'c', lw=3, label=r'$\mathrm{Orbit\ Calculation}$')
 ax[0][0].plot(mt_x, mt_y, 'g', lw=3, label=r'$\mathrm{Merger\ Tree}$')
 ax[0][0].plot(0,0,'^k',markersize=12,label=r'$\mathrm{Host\ Center}$')
 ax[0][0].plot(x[0],y[0],'c*',markersize=15,label=r'$\mathrm{Orbit\ Start}$')
-if compare: ax[0][0].plot(x_d, y_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if drag: ax[0][0].plot(x_d, y_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if triaxial: ax[0][0].plot(x_tri, y_tri, 'k--', lw=3, label=r'$\mathrm{Triaxial}$')
 ax[0][0].set_xlabel(r'$\mathrm{x\ (Mpc)}$')
 ax[0][0].set_ylabel(r'$\mathrm{y\ (Mpc)}$')
 # ax[0][0].legend(loc='lower left',fontsize=15)
@@ -108,7 +137,8 @@ ax[0][1].plot(y, z, 'c', lw=3, label=r'$\mathrm{Orbit\ Calculation}$')
 ax[0][1].plot(mt_y, mt_z, 'g', lw=3, label=r'$\mathrm{Merger\ Tree}$')
 ax[0][1].plot(0,0,'^k',markersize=12,label=r'$\mathrm{Host\ Center}$')
 ax[0][1].plot(y[0],z[0],'c*',markersize=15,label=r'$\mathrm{Orbit\ Start}$')
-if compare: ax[0][1].plot(y_d, z_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if drag: ax[0][1].plot(y_d, z_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if triaxial: ax[0][1].plot(y_tri, z_tri, 'k--', lw=3, label=r'$\mathrm{Triaxial}$')
 ax[0][1].set_xlabel(r'$\mathrm{y\ (Mpc)}$')
 ax[0][1].set_ylabel(r'$\mathrm{z\ (Mpc)}$')
 # ax[0][1].legend(loc='lower left',fontsize=15)
@@ -119,7 +149,8 @@ ax[0][2].plot(z, x, 'c', lw=3, label=r'$\mathrm{Orbit\ Calculation}$')
 ax[0][2].plot(mt_z, mt_x, 'g', lw=3, label=r'$\mathrm{Merger\ Tree}$')
 ax[0][2].plot(0,0,'^k',markersize=12,label=r'$\mathrm{Host\ Center}$')
 ax[0][2].plot(z[0],x[0],'c*',markersize=15,label=r'$\mathrm{Orbit\ Start}$')
-if compare: ax[0][2].plot(z_d, x_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if drag: ax[0][2].plot(z_d, x_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if triaxial: ax[0][2].plot(z_tri, x_tri, 'k--', lw=3, label=r'$\mathrm{Triaxial}$')
 ax[0][2].set_xlabel(r'$\mathrm{z\ (Mpc)}$')
 ax[0][2].set_ylabel(r'$\mathrm{x\ (Mpc)}$')
 # ax[0][2].legend(loc='lower left',fontsize=15)
@@ -131,7 +162,8 @@ ax[0][2].set_ylim([-2,2])
 ax[1][0].plot(t, vx, 'c', lw=3, label=r'$\mathrm{Orbit\ Calculation}$')
 ax[1][0].plot(mt_t, mt_vx, 'g', lw=3, label=r'$\mathrm{Merger\ Tree}$')
 ax[1][0].plot(t[0],vx[0],'c*',markersize=15,label=r'$\mathrm{Orbit\ Start}$')
-if compare: ax[1][0].plot(t, vy_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if drag: ax[1][0].plot(t, vy_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if triaxial: ax[1][0].plot(t, vy_tri, 'k--', lw=3, label=r'$\mathrm{Drag}$')
 ax[1][0].set_xlabel(r'$\mathrm{t\ (Gyr)}$')
 ax[1][0].set_ylabel(r'$\mathrm{vy\ (Mpc/Gyr)}$')
 # ax[1][0].legend(loc='lower left',fontsize=15)
@@ -139,7 +171,8 @@ ax[1][0].set_ylabel(r'$\mathrm{vy\ (Mpc/Gyr)}$')
 ax[1][1].plot(t, vy, 'c', lw=3, label=r'$\mathrm{Orbit\ Calculation}$')
 ax[1][1].plot(mt_t, mt_vy, 'g', lw=3, label=r'$\mathrm{Merger\ Tree}$')
 ax[1][1].plot(t[0],vy[0],'c*',markersize=15,label=r'$\mathrm{Orbit\ Start}$')
-if compare: ax[1][1].plot(t, vy_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if drag: ax[1][1].plot(t, vy_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if triaxial: ax[1][1].plot(t, vy_tri, 'k--', lw=3, label=r'$\mathrm{Triaxial}$')
 ax[1][1].set_xlabel(r'$\mathrm{t\ (Gyr)}$')
 ax[1][1].set_ylabel(r'$\mathrm{vy\ (Mpc/Gyr)}$')
 # ax[1][1].legend(loc='lower left',fontsize=15)
@@ -147,7 +180,8 @@ ax[1][1].set_ylabel(r'$\mathrm{vy\ (Mpc/Gyr)}$')
 ax[1][2].plot(t, vz, 'c', lw=3, label=r'$\mathrm{Orbit\ Calculation}$')
 ax[1][2].plot(mt_t, mt_vz, 'g', lw=3, label=r'$\mathrm{Merger\ Tree}$')
 ax[1][2].plot(t[0],vz[0],'c*',markersize=15,label=r'$\mathrm{Orbit\ Start}$')
-if compare: ax[1][2].plot(t, vz_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if drag: ax[1][2].plot(t, vz_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if triaxial: ax[1][2].plot(t, vz_tri, 'k--', lw=3, label=r'$\mathrm{Triaxial}$')
 ax[1][2].set_xlabel(r'$\mathrm{t\ (Gyr)}$')
 ax[1][2].set_ylabel(r'$\mathrm{vy\ (Mpc/Gyr)}$')
 # ax[1][2].legend(loc='lower left',fontsize=15)
@@ -157,7 +191,8 @@ ax[1][2].set_ylabel(r'$\mathrm{vy\ (Mpc/Gyr)}$')
 ax[2][1].plot(t,dist,'c',lw=3, label=r'$\mathrm{Orbit\ Calculation}$')
 ax[2][1].plot(mt_t, mt_dist, 'g', lw=3, label=r'$\mathrm{Merger\ Tree}$')
 ax[2][1].plot(t[0],dist[0],'c*',markersize=15,label=r'$\mathrm{Orbit\ Start}$')
-if compare: ax[2][1].plot(t, dist_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if drag: ax[2][1].plot(t, dist_d, 'b--', lw=3, label=r'$\mathrm{Drag}$')
+if triaxial: ax[2][1].plot(t, dist_tri, 'k--', lw=3, label=r'$\mathrm{Triaxial}$')
 ax[2][1].set_xlabel(r'$\mathrm{t\ (Gyr)}$')
 ax[2][1].set_ylabel(r'$\mathrm{r\ (Mpc)}$')
 
