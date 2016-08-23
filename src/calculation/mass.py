fixed units for comparison to merger tree

src/calculation/mass.py
@@ -39,7 +39,7 @@ def triaxial_integrand(phi, theta, r, host):
     return triaxial_NFW_density(r*cosph*sinth, r*sinph*sinth, r*costh, host) * sinth*r2
 '''
 def calculate_triaxial_mass(host,a,b):
-    t0 = time.time()
+    # t0 = time.time()
     # func = lambda phi, theta, r: triaxial_NFW_density(r*np.cos(phi)*np.sin(theta), r*np.sin(phi)*np.sin(theta), r*np.cos(theta), host) * np.sin(theta)*r*r
     def integrand(phi, theta, r):
         sinth = np.sin(theta)
@@ -59,8 +59,8 @@ def calculate_triaxial_mass(host,a,b):
 
     # res = tplquad(func,a,b,gfun,hfun,qfun,rfun)[0]
     res = tplquad(integrand, a, b, gfun, hfun, qfun, rfun, epsabs=epsabs, epsrel=epsrel)[0]
-    t1 = time.time()
-    print 'time passed = %.2f seconds' %(t1-t0)
+    # t1 = time.time()
+    # print 'time passed = %.2f seconds' %(t1-t0)
     return res
 
 def calculate_spherical_mass(host,a,b):

src/halos/subhalo.py
@@ -45,8 +45,8 @@ class Subhalo:
 
 	def update(self, time):
 		self.M = self.M_sp(time)
-                self.calculate_energy()
-                self.distance_array.append(self.distance())
+		self.calculate_energy()
+		self.distance_array.append(self.distance())
 
-        def distance(self):
-            return np.sqrt(self.position[0]**2 + self.position[1]**2 + self.position[2]**2)
+	def distance(self):
+		return np.sqrt(self.position[0]**2 + self.position[1]**2 + self.position[2]**2)

src/plotting/gala_orbit.py
@@ -24,7 +24,7 @@ plt.rc('ytick.minor', pad=5)
 
 host_idx = int(sys.argv[1])
 # potential = 'spherical_NFW'
-potential = 'spherical_NFW'
+potential = 'triaxial_NFW'
 dt = 4e-3
 subs = np.array(sys.argv[2:],dtype=int)
 
@@ -34,8 +34,11 @@ host.update(host.cosmo.age(0)) # potential will not be evolving
 usys = UnitSystem(u.Mpc, u.Gyr, u.Msun, u.degree, u.Mpc/u.Gyr)
 
 M = host.mass_function(host=host, a=0, b=host.R_s)
-v = np.sqrt(G*M/host.R_s)
 R = host.R_s
+
+gravity = GetGravitationalForce(host)
+fg, _ = gravity.calculate_partial_force([host.R_s,0,0])
+v = np.sqrt(fg*host.R_s)
 
 if host.potential == 'spherical_NFW':
 	pot = gp.SphericalNFWPotential(v_c = v*u.Mpc/u.Gyr, r_s=R*u.Mpc, units=usys)

src/plotting/plot_subhalo.py
@@ -63,9 +63,10 @@ sub = subs[sub_idx]
 mt_t = cosmo.age(1/sub.a-1)
 # print mt_t[0],mt_t[-1]
 
-mt_x = sub.rel_x
-mt_y = sub.rel_y
-mt_z = sub.rel_z
+h = 0.7
+mt_x = sub.rel_x/(h*(1/sub.a))
+mt_y = sub.rel_y/(h*(1/sub.a))
+mt_z = sub.rel_z/(h*(1/sub.a))
 
 mt_vx = sub.rel_vx*1000*m_to_Mpc/s_to_Gyr
 mt_vy = sub.rel_vy*1000*m_to_Mpc/s_to_Gyr
@@ -99,7 +100,7 @@ if drag:
 triaxial = 0
 if triaxial:
 	integrator = 'leapfrog'
-	potential = 'triaxial_NFW_BT'
+	potential = 'triaxial_NFW'
 	infile_triaxial = HOMEDIR+'sidm_orbit_calculation/src/output/%i_%s_%s_%.0e_%i.dat' %(host_idx,integrator,potential,dt,sub_idx)
 	f = open(infile_triaxial,'rb')
 	data = cPickle.load(f)
@@ -117,7 +118,7 @@ if triaxial:
 	vy_tri = velocities_tri[:,1]
 	vz_tri = velocities_tri[:,2]
 
-gala = 1
+gala = 0
 if gala:
 	f = open(HOMEDIR + 'sidm_orbit_calculation/src/output/gala_orbit_%i_spherical_NFW_%.0e_%i.dat'%(host_idx,dt,sub_idx))
 	orbit = cPickle.load(f)
@@ -134,7 +135,7 @@ if gala:
 
 	dist_g = np.sqrt(x_g**2 + y_g**2 + z_g**2)
 
-gala_tri = 1
+gala_tri = 0
 if gala_tri:
 	f = open(HOMEDIR + 'sidm_orbit_calculation/src/output/gala_orbit_%i_triaxial_NFW_%.0e_%i.dat'%(host_idx,dt,sub_idx))
 	orbit = cPickle.load(f)
@@ -151,7 +152,7 @@ if gala_tri:
 
 	dist_gt = np.sqrt(x_gt**2 + y_gt**2 + z_gt**2)
 
-constant = 1
+constant = 0
 if constant:
 	integrator = 'leapfrog'
 	potential = 'triaxial_NFW_BT'