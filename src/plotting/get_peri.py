2f681a2c115b049826ac107ea49d4cbcc8dafea7
Nora Shipp <norashipp@uchicago.edu>
2016-09-26 12:51:56 -0500

    midawy plotting

--- src/plotting/get_peri.py
+++ src/plotting/get_peri.py
@@ -24,10 +24,7 @@ plt.rc('ytick.minor', pad=5)
 def get_radii(r):
 	minima = np.r_[True, r[1:] < r[:-1]] & np.r_[r[:-1] < r[1:], True]
 	maxima = np.r_[True, r[1:] > r[:-1]] & np.r_[r[:-1] > r[1:], True]
-	# print r[minima]
-	# print r[maxima]
 	if len(r[minima]) < 2 or len(r[maxima]) < 2:
-		# print 'problem..'
 		return None, None
 	else:
 		if r[1] < r[0]:
@@ -37,15 +34,11 @@ def get_radii(r):
 			rp = r[minima][0]
 			ra = r[maxima][1]
 		return rp, ra
-'''
 # last
-def get_radii(r):
+def get_final_radii(r):
 	minima = np.r_[True, r[1:] < r[:-1]] & np.r_[r[:-1] < r[1:], True]
 	maxima = np.r_[True, r[1:] > r[:-1]] & np.r_[r[:-1] > r[1:], True]
-	# print r[minima]
-	# print r[maxima]
 	if len(r[minima]) < 2 or len(r[maxima]) < 2:
-		# print 'problem..'
 		return None, None
 	else:
 		if r[-1] < r[-2]:
@@ -55,7 +48,6 @@ def get_radii(r):
 			rp = r[minima][-1]
 			ra = r[maxima][-2]
 		return rp, ra
-'''
 
 sub_dict = {}
 hosts = np.array(sys.argv[1:],dtype=int)
@@ -71,35 +63,57 @@ v_thresh = 100 # km/s
 nbins = 12
 rbins = np.logspace(-4,-1,nbins+1)
 
-sigs = [0,3,9,15,21]
-colors = ['b','g','r','c','m','y','k','w']
-for k, sig in enumerate(sigs):
+sig = 0
+apo = []
+peri = []
+subs = []
+for j, host_idx in enumerate(hosts):
+	host = HostHalo(host_idx,potential,subs=True,scale_density=False)
+	subhalos = np.copy(host.subhalos)
+	sub_dict[j] = subhalos
+	host.update(host.cosmo.age(0))
+	print 'Host %i' %host_idx
+	print 'M = %.2e' %host.M
+	for sub_idx, sub in enumerate(subhalos):
+		if sub:
+			infile = HOMEDIR+'data/candidacy/sigma%i/%i_%s_%.0e_%.2f_%i.dat' %(sig,host_idx,potential,dt,sig,sub_idx)
+			f = open(infile,'rb')
+			data = cPickle.load(f)
+			f.close()
+			_, positions, _ = data
+			d = np.sqrt(positions[:,0]**2 + positions[:,1]**2 + positions[:,2]**2)
+			rp,ra = get_radii(d)
+			if rp == None or ra == None:
+				continue
+			else:
+				subs.append(sub_idx)
+				apo.append(ra)
+				peri.append(rp)
+np.savetxt('output/apocenter_%s_%.0e_sigma_%.2f.txt' %(potential, dt, sig),apo)
+np.savetxt('output/pericenter_%s_%.0e_sigma_%.2f.txt' %(potential, dt, sig),peri)
+
+sigs = [3,9,15,21]
+for sig in sigs:
 	apo = []
 	peri = []
 	for j, host_idx in enumerate(hosts):
-		if sig == sigs[0]:
-			host = HostHalo(host_idx,potential,subs=True,scale_density=False)
-			subhalos = np.copy(host.subhalos)
-			sub_dict[j] = subhalos
-		else:
-			host = HostHalo(host_idx,potential,subs=False,scale_density=False)
-			subhalos = sub_dict[j]
+		host = HostHalo(host_idx,potential,subs=False,scale_density=False)
+		subhalos = sub_dict[j]
 		host.update(host.cosmo.age(0))
 		print 'Host %i' %host_idx
 		print 'M = %.2e' %host.M
 		
-		for sub_idx, sub in enumerate(subhalos):
-			if sub:
-				infile = HOMEDIR+'data/candidacy/sigma%i/%i_%s_%.0e_%.2f_%i.dat' %(sig,host_idx,potential,dt,sig,sub_idx)
-				f = open(infile,'rb')
-				data = cPickle.load(f)
-				f.close()
-				_, positions, _ = data
-				d = np.sqrt(positions[:,0]**2 + positions[:,1]**2 + positions[:,2]**2)
-				rp,ra = get_radii(d)
-				# if not rp: continue
-				apo.append(ra)
-				peri.append(rp)
+		for sub_idx in subs:
+			sub = subhalos[sub_idx]
+			infile = HOMEDIR+'data/candidacy/sigma%i/%i_%s_%.0e_%.2f_%i.dat' %(sig,host_idx,potential,dt,sig,sub_idx)
+			f = open(infile,'rb')
+			data = cPickle.load(f)
+			f.close()
+			_, positions, _ = data
+			d = np.sqrt(positions[:,0]**2 + positions[:,1]**2 + positions[:,2]**2)
+			rp,ra = get_radii(d)
+			apo.append(ra)
+			peri.append(rp)
 
-	np.savetxt('output/first_apocenter_%s_%.0e_sigma_%.2f.txt' %(potential, dt, sig),apo)
-	np.savetxt('output/first_pericenter_%s_%.0e_sigma_%.2f.txt' %(potential, dt, sig),peri)
+	np.savetxt('output/apocenter_%s_%.0e_sigma_%.2f.txt' %(potential, dt, sig),apo)
+	np.savetxt('output/pericenter_%s_%.0e_sigma_%.2f.txt' %(potential, dt, sig),peri)
